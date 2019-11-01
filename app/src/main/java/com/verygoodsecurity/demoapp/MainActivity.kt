8d357673ca44650a6a2836cc036c094f57021f0f
Endorf <dmitriy.kosolapenko@gmail.com>
2019-11-01 16:12:37 +0400

    update attribute list for EditText

--- app/src/main/java/com/verygoodsecurity/demoapp/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoapp/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.verygoodsecurity.demoapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import com.verygoodsecurity.vgscollect.core.Environment
 import com.verygoodsecurity.vgscollect.core.VGSCollect
 import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
 import com.verygoodsecurity.vgscollect.core.data.SimpleResponse
+import com.verygoodsecurity.vgscollect.widget.VGSEditText
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), VgsCollectResponseListener {
 
@@ -23,18 +30,13 @@ class MainActivity : AppCompatActivity(), VgsCollectResponseListener {
         }
 
         vgsForm.onResponceListener = this
-
         vgsForm.bindView(cardCVVField)
-        vgsForm.bindView(cardHolderField)
-        vgsForm.bindView(cardNumberField)
-        vgsForm.bindView(expDateField)
+        setTruncateAt(cardCVVField)
     }
-
     override fun onDestroy() {
         vgsForm.onDestroy()
         super.onDestroy()
     }
-
     override fun onResponse(response: SimpleResponse?) {
         progressBar?.visibility = View.INVISIBLE
         response?.let {
@@ -42,5 +44,35 @@ class MainActivity : AppCompatActivity(), VgsCollectResponseListener {
 
             Log.e("------->", "${response.code} \n ${response.responce}")
         }
+    }
+
+
+
+    private fun setTruncateAt(textView:VGSEditText) {
+        textView.setEllipsize(TextUtils.TruncateAt.START)
+        textView.setMaxLines(2)
+    }
+    private fun setHintTextColor(textView:VGSEditText) {
+        textView.setHintTextColor(Color.BLUE)
+    }
+    private fun canScrollHorizontally(textView:VGSEditText) {
+        textView.canScrollHorizontally(true)
+    }
+    private fun setGravity(textView:VGSEditText) {
+        textView.setGravity(Gravity.CENTER_VERTICAL or Gravity.RIGHT)
+    }
+    private fun setTextAppearance(textView:VGSEditText) {
+        textView.setTextAppearance(android.R.style.TextAppearance_Large)
+
+        textView.setTextAppearance(this, android.R.style.TextAppearance_Large)
+    }
+    private fun setFonts(textView:VGSEditText) {
+        textView.setTypeface(Typeface.DEFAULT_BOLD)
+
+        val mtypeFace = Typeface.createFromAsset(assets, "barethos1.ttf")
+        textView.setTypeface(mtypeFace)
+
+        val typeface = ResourcesCompat.getFont(this, R.font.barethos2)!!
+        textView.setTypeface(typeface)
     }
 }
