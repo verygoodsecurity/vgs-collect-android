c8ddd64f1f744db4d51c4e3fb14a53b59d76a945
Endorf <dmitriy.kosolapenko@gmail.com>
2019-11-13 13:24:29 +0400

    add internal isRoot method

---
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/CommonUtil.kt
@@ -0,0 +1,33 @@
+package com.verygoodsecurity.vgscollect.util
+
+import java.io.File
+
+internal object CommonUtil {
+    fun isRooted():Boolean {
+        val buildTags = android.os.Build.TAGS
+        if (buildTags != null && buildTags.contains("test-keys")) {
+            return true
+        }
+
+        // check if /system/app/Superuser.apk is present
+        try {
+            val file = File("/system/app/Superuser.apk")
+            if (file.exists()) {
+                return true
+            }
+        } catch (e1: Exception) {
+            // ignore
+        }
+
+        return canExecuteCommand("su")
+    }
+
+    private fun canExecuteCommand(command:String):Boolean {
+        return try {
+            Runtime.getRuntime().exec(command)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
