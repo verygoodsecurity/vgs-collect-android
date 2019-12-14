c6dcefc7330c4740b7078198902d65dbc86a9a02
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2019-12-14 22:06:54 +0200

    fix add custom brand

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/filter/CardBrandFilter.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/filter/CardBrandFilter.kt
@@ -2,18 +2,26 @@ package com.verygoodsecurity.vgscollect.view.card.filter
 
 import android.text.InputFilter
 import android.widget.TextView
+import com.verygoodsecurity.vgscollect.R
 import com.verygoodsecurity.vgscollect.view.card.CustomCardBrand
 import java.util.regex.Pattern
 
 class CardBrandFilter(
-    private val userCustomCardBrands: Array<CustomCardBrand>,
     private val inputField: TextView?,
     private val divider:String? = ""
-) : VGSCardFilter {
+) : MutableCardFilter {
+
+    private val userCustomCardBrands = mutableListOf<CustomCardBrand>()
 
     companion object {
         private const val MAX_LENGTH = 19
         private const val MASK_COUNT = 3
+    }
+
+    override fun add(item: CustomCardBrand?) {
+        item?.let {
+            userCustomCardBrands.add(item)
+        }
     }
 
     override fun detect(str: String?): CardBrandWrapper? {
@@ -27,7 +35,8 @@ class CardBrandFilter(
             val m = Pattern.compile(value.regex).matcher(preparedStr)
             while (m.find()) {
                 inputField?.filters = arrayOf(InputFilter.LengthFilter(MAX_LENGTH + MASK_COUNT))
-                return CardBrandWrapper(regex = value.regex, name = value.cardBrandName, resId = value.drawableResId)
+                val s = R.drawable.cirrus
+                return CardBrandWrapper(regex = value.regex, name = value.cardBrandName, resId = s)
             }
         }
 
