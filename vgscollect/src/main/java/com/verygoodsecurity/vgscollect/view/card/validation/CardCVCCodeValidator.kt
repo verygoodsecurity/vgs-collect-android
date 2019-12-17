70b2931d00e49f67238a3b787303bdca0dacd792
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2019-12-17 16:27:46 +0200

    update validator interface

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardCVCCodeValidator.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardCVCCodeValidator.kt
@@ -1,10 +1,6 @@
 package com.verygoodsecurity.vgscollect.view.card.validation
 
 class CardCVCCodeValidator:VGSValidator {
-    override fun clearRules() {}
-
-    override fun addRule(regex: String) {}
-
     override fun isValid(content: String?): Boolean {
         val data:Int? =  content?.trim()?.toIntOrNull()
         return data != null && data >= 3
--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardHolderValidator.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardHolderValidator.kt
@@ -4,9 +4,6 @@ import java.util.regex.Pattern
 
 class CardHolderValidator:VGSValidator {
     private val m = Pattern.compile("^[a-zA-Z0-9 ,]+\$")      //only symbols  -  "^[\\p{L}\\s'.-]+\$"
-    override fun clearRules() {}
-
-    override fun addRule(regex: String) {}
 
     override fun isValid(content: String?): Boolean {
         val str = content?.trim()
