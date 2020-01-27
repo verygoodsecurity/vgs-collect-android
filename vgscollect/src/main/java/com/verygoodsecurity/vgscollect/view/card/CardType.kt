07ec7f8371ed1a4a2e499462b5e686d05aabbdb0
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-01-27 18:00:34 +0200

    update images

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/CardType.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/CardType.kt
@@ -8,19 +8,19 @@ enum class CardType(val regex:String,
                     val rangeCVV:Array<Int>) {
     ELO(
         "^(4011(78|79)|43(1274|8935)|45(1416|7393|763(1|2))|50(4175|6699|67[0-7][0-9]|9000)|627780|63(6297|6368)|650(03([^4])|04([0-9])|05(0|1)|4(0[5-9]|3[0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8])|9([2-6][0-9]|7[0-8])|541|700|720|901)|651652|655000|655021)",
-        R.drawable.card_preview,
+        R.drawable.ic_card_front_preview_dark,
         arrayOf(16),
         arrayOf(3)
     ),
     VISA_ELECTRON(
         "^4(026|17500|405|508|844|91[37])",
-        R.drawable.ic_visa_electron_lite,
+        R.drawable.ic_visa_electron_dark,
         arrayOf(16),
         arrayOf(3)
     ),
     LASER(
         "^(6706|6771|6709|6304)",
-        R.drawable.card_preview,
+        R.drawable.ic_card_front_preview_dark,
         (16..19).toList().toTypedArray(),
         arrayOf(3)
     ),
@@ -62,7 +62,7 @@ enum class CardType(val regex:String,
     ),
     HIPERCARD(
         "^(384100|384140|384160|606282|637095|637568|60(?!11))",
-        R.drawable.card_preview,
+        R.drawable.ic_card_front_preview_dark,
         (14..19).toList().toTypedArray(),
         arrayOf(3)
     ),
@@ -74,26 +74,26 @@ enum class CardType(val regex:String,
     ),
     DISCOVER(
         "^(6011|65|64[4-9]|622)",
-        R.drawable.ic_discover_lite,
+        R.drawable.ic_discover_light,
         arrayOf(16),
         arrayOf(3)
     ),
     UNIONPAY(
         "^62",
-        R.drawable.ic_union_pay_lite,
+        R.drawable.ic_union_pay,
         (16..19).toList().toTypedArray(),
         arrayOf(3)
     ),
     JCB(
         "^35",
-        R.drawable.ic_jcb_lite,
+        R.drawable.ic_jcb_light,
         (16..19).toList().toTypedArray(),
         arrayOf(3)
     ),
 
     NONE(
         "^\$a",
-        R.drawable.card_preview,
+        R.drawable.ic_card_front_preview_dark,
         (16..19).toList().toTypedArray(),
         arrayOf(3,4)
     );
