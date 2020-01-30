ca511d96657e6ac1d221a5cd3267b8476e3e2972
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-01-30 17:32:48 +0200

    update javadoc for data classes [ch59265]

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/VGSResponse.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/VGSResponse.kt
@@ -1,9 +1,28 @@
 package com.verygoodsecurity.vgscollect.core.model
 
+/**
+ * The base class definition for a VGSCollect response states.
+ *
+ * @param code The response code from server.
+ *
+ * @version 1.0.1
+ */
 sealed class VGSResponse(val code:Int = -1) {
 
+    /**
+     * The class definition for a success response state.
+     *
+     * @param response The response code from server.
+     * @param successCode The response code from server.
+     */
     class SuccessResponse(val response:Map<String, String>? = null, val successCode:Int = -1):VGSResponse(successCode)
 
+    /**
+     * The class definition for an error response state.
+
+     * @param localizeMessage The message of the error.
+     * @param errorCode The response code from server.
+     */
     class ErrorResponse(val localizeMessage:String? = "", val errorCode:Int = -1):VGSResponse(errorCode)
 
 }
