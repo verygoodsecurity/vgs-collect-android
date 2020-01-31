46d6a56ae60dcc81ff5ff402a7f01e2922048fe0
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-01-31 16:37:24 +0200

    update javadoc [ch59265]

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/state/Dependency.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/state/Dependency.kt
@@ -2,4 +2,10 @@ package com.verygoodsecurity.vgscollect.core.model.state
 
 import com.verygoodsecurity.vgscollect.core.storage.DependencyType
 
+/**
+ *
+ *
+ * @param dependencyType
+ * @param value
+ */
 data class Dependency(val dependencyType: DependencyType = DependencyType.LENGTH, val value:Int)
--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/storage/DependencyListener.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/storage/DependencyListener.kt
@@ -2,6 +2,14 @@ package com.verygoodsecurity.vgscollect.core.storage
 
 import com.verygoodsecurity.vgscollect.core.model.state.Dependency
 
+/**
+ *
+ */
 interface DependencyListener {
+
+    /**
+     *
+     * @param dependency
+     */
     fun dispatchDependencySetting(dependency: Dependency)
 }
