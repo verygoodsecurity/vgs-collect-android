7f1b395f44699bc334519be1a8595ccd8ae74d97
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-03-24 19:47:29 +0200

    merge file storage with fields [ch61923]

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/storage/content/file/Base64Cipher.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/storage/content/file/Base64Cipher.kt
@@ -4,11 +4,15 @@ import android.app.Activity
 import android.content.Context
 import android.net.Uri
 import android.util.Base64
-import java.io.ByteArrayOutputStream
-import java.util.HashMap
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 
 internal class Base64Cipher(context: Context):VgsFileCipher {
     private val contentResolver = (context as Activity).contentResolver
+    private val outputDir = context.cacheDir
     private var submitCode = -1L
     private var fieldName = ""
 
@@ -40,23 +44,51 @@ internal class Base64Cipher(context: Context):VgsFileCipher {
         }?:""
     }
 
-    private fun getFile(fileUri: Uri): ByteArray? {
+    fun getFile(fileUri: Uri): ByteArray? {
         var ret: ByteArray? = null
+//            val tempFile = File.createTempFile(
+//                "splitName[0]",
+//                "jpg"
+//            )
+//            contentResolver.takePersistableUriPermission(
+//                fileUri,
+//                Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+//            )
+        val tempFile = File.createTempFile("prefix", "extension", outputDir)
 
-        contentResolver?.openInputStream(fileUri)?.use { inputStream->
-            val outputStream = ByteArrayOutputStream()
-
-            var nextByte = inputStream.read()
-            while (nextByte != -1) {
-                outputStream.write(nextByte)
-                nextByte = inputStream.read()
-            }
-
-            ret = outputStream.toByteArray()
-            outputStream.close()
+        val inputStream: InputStream = contentResolver.openInputStream(fileUri)!!
+        val out: OutputStream = FileOutputStream(tempFile)
+        val buf = ByteArray(1024)
+        var len = 0
+        while (inputStream.read(buf).also { len = it } > 0) {
+            out.write(buf, 0, len)
         }
+        out.close()
+        inputStream.close()
 
-        return ret
+//        contentResolver?.openInputStream(fileUri)?.use { inputStream->
+//            val outputStream = ByteArrayOutputStream()
+//
+//            var nextByte = inputStream.read()
+//            while (nextByte != -1) {
+//                outputStream.write(nextByte)
+//                nextByte = inputStream.read()
+//            }
+//
+//            ret = outputStream.toByteArray()
+//            outputStream.close()
+////                FileOutputStream(tempFile).use { outputStream->
+////                    val buf = ByteArray(1024)
+////                    var len = 0
+////                    while (inputStream.read(buf).also { len = it } > 0) {
+////                        outputStream.write(buf, 0, len)
+////                    }
+////                }
+//        }
+
+//            val inputStream: InputStream = contentResolver.openInputStream(fileUri)
+
+        return tempFile.readBytes()
     }
 
     override fun getBase64(file: Uri): String {
