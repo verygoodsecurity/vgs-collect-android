addc13f3c0e18047fd67e56b1ca30a43727776dd
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-03-25 20:08:37 +0200

    update VGSCollect unitTests [ch61923]

--- vgscollect/src/test/java/com/verygoodsecurity/vgscollect/VGSCollectTest.kt
+++ vgscollect/src/test/java/com/verygoodsecurity/vgscollect/VGSCollectTest.kt
@@ -1,95 +1,255 @@
 package com.verygoodsecurity.vgscollect
 
-import com.verygoodsecurity.vgscollect.core.*
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import com.verygoodsecurity.vgscollect.app.BaseTransmitActivity
+import com.verygoodsecurity.vgscollect.core.HTTPMethod
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
 import com.verygoodsecurity.vgscollect.core.api.ApiClient
 import com.verygoodsecurity.vgscollect.core.api.VgsApiTemporaryStorageImpl
-import com.verygoodsecurity.vgscollect.core.storage.VgsStore
+import com.verygoodsecurity.vgscollect.core.model.VGSHashMapWrapper
+import com.verygoodsecurity.vgscollect.core.model.VGSRequest
+import com.verygoodsecurity.vgscollect.core.storage.InternalStorage
+import com.verygoodsecurity.vgscollect.core.storage.OnFieldStateChangeListener
+import com.verygoodsecurity.vgscollect.core.storage.content.file.VGSContentProviderImpl
+import com.verygoodsecurity.vgscollect.view.card.FieldType
+import com.verygoodsecurity.vgscollect.view.card.material.TestApplication
+import com.verygoodsecurity.vgscollect.widget.VGSEditText
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.android.controller.ActivityController
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P], application = TestApplication::class)
 class VGSCollectTest {
+    private lateinit var activityController: ActivityController<Activity>
+    private lateinit var activity: Activity
+
+    private lateinit var collect: VGSCollect
+
+    @Before
+    fun setUp() {
+        activityController = Robolectric.buildActivity(Activity::class.java)
+        activity = activityController.get()
+        collect = VGSCollect(activity, "tnts")
+
+        val activityShadow = Shadows.shadowOf(activity)
+        activityShadow.grantPermissions(
+            Manifest.permission.INTERNET
+        )
+    }
 
     @Test
-    fun testOnDestroyTest() {
-        val store = Mockito.mock(VgsStore::class.java)
+    fun test_add_response_listener() {
+        applyResponseListener()
+        applyResponseListener()
 
-        val collect = VGSCollect("tnts")
-        collect.setStorage(store)
+        assertEquals(2, collect.getResponseListeners().size)
+    }
+
+    @Test
+    fun test_add_state_change_listener() {
+        val storage = applyStorage()
+
+        applyStateChangeListener()
+        applyStateChangeListener()
+
+        Mockito.verify(storage, Mockito.times(2)).attachStateChangeListener(any())
+    }
+
+    @Test
+    fun test_bind_view() {
+        val view = applyEditText(FieldType.INFO)
+
+        Mockito.verify(view).getFieldType()
+        Mockito.verify(view).getFieldName()
+        Mockito.verify(view).addStateListener(any())
+    }
+
+    @Test
+    fun test_on_destroy() {
+        applyResponseListener()
+
+        val storage = applyStorage()
 
         collect.onDestroy()
-        Mockito.verify(store).clear()
-    }
 
-
-    @Test
-    fun testGetAllStates() {
-        val store = Mockito.mock(VgsStore::class.java)
-
-        val collect = VGSCollect("tnts")
-        collect.setStorage(store)
-
-        collect.getAllStates()
-
-        Mockito.verify(store).getStates()
+        assertEquals(0, collect.getResponseListeners().size)
+        Mockito.verify(storage).clear()
     }
 
     @Test
-    fun testSetCustomData() {
-        val client = Mockito.mock(ApiClient::class.java)
+    fun test_get_all_states() {
+        applyEditText(FieldType.CARD_NUMBER)
+        applyEditText(FieldType.CVC)
+
+        assertEquals(2, collect.getAllStates().size)
+    }
+
+    @Test
+    fun test_submit_path_method() {
+        val client = applyApiClient()
+
+        collect.submit("/path", HTTPMethod.POST)
+
+        Mockito.verify(client).call(any(), any(), any(), any())
+    }
+
+    @Test
+    fun test_submit_request_builder() {
+        val client = applyApiClient()
+
+        val request = VGSRequest.VGSRequestBuilder()
+            .setPath("/path")
+            .setMethod(HTTPMethod.POST)
+            .build()
+        collect.submit(request)
+
+        Mockito.verify(client).call(any(), any(), any(), any())
+    }
+
+    @Test
+    fun test_asyncsubmit_path_method() {
+        val client = applyApiClient()
+
+        collect.asyncSubmit("/path", HTTPMethod.POST)
+
+        Mockito.verify(client).call(any(), any(), any(), any())
+    }
+
+    @Test
+    fun test_asyncsubmit_request_builder() {
+        val client = applyApiClient()
+
+        val request = VGSRequest.VGSRequestBuilder()
+            .setPath("/path")
+            .setMethod(HTTPMethod.POST)
+            .build()
+        collect.asyncSubmit(request)
+
+        Mockito.verify(client).call(any(), any(), any(), any())
+    }
+
+    @Test
+    fun test_on_activity_result_pick_file() {
+        val storage = applyStorage()
+
+        val intent = Intent()
+        val map = VGSHashMapWrapper<String, Any?>()
+        intent.putExtra(BaseTransmitActivity.RESULT_DATA, map)
+
+        collect.onActivityResult(VGSContentProviderImpl.REQUEST_CODE, Activity.RESULT_OK, intent)
+
+        Mockito.verify(storage).getFileStorage()
+    }
+
+    @Test
+    fun test_set_custom_headers() {
+        val client = applyApiClient()
         Mockito.doReturn(VgsApiTemporaryStorageImpl())
             .`when`(client).getTemporaryStorage()
-
-        val collect = VGSCollect("tnts")
-        collect.setClient(client)
 
         val data = HashMap<String, String>()
         data["key"] = "value"
         collect.setCustomHeaders(data)
 
         Mockito.verify(client).getTemporaryStorage()
+        assertEquals(1, client.getTemporaryStorage().getCustomHeaders().size)
     }
 
     @Test
-    fun testSetCustomHeaders() {
-        val client = Mockito.mock(ApiClient::class.java)
+    fun test_reset_custom_headers() {
+        val client = applyApiClient()
         Mockito.doReturn(VgsApiTemporaryStorageImpl())
             .`when`(client).getTemporaryStorage()
-
-        val collect = VGSCollect("tnts")
-        collect.setClient(client)
-
-        val data = HashMap<String, String>()
-        data["key"] = "value"
-        collect.setCustomHeaders(data)
-
-        Mockito.verify(client).getTemporaryStorage()
-    }
-
-    @Test
-    fun testResetCustomData() {
-        val client = Mockito.mock(ApiClient::class.java)
-        Mockito.doReturn(VgsApiTemporaryStorageImpl())
-            .`when`(client).getTemporaryStorage()
-
-        val collect = VGSCollect("tnts")
-        collect.setClient(client)
-
-        collect.resetCustomData()
-
-        Mockito.verify(client).getTemporaryStorage()
-    }
-
-    @Test
-    fun testResetCustomHeaders() {
-        val client = Mockito.mock(ApiClient::class.java)
-        Mockito.doReturn(VgsApiTemporaryStorageImpl())
-            .`when`(client).getTemporaryStorage()
-
-        val collect = VGSCollect("tnts")
-        collect.setClient(client)
 
         collect.resetCustomHeaders()
 
         Mockito.verify(client).getTemporaryStorage()
+        assertEquals(0, client.getTemporaryStorage().getCustomHeaders().size)
+    }
+
+    @Test
+    fun test_set_custom_data() {
+        val client = applyApiClient()
+        Mockito.doReturn(VgsApiTemporaryStorageImpl())
+            .`when`(client).getTemporaryStorage()
+
+        val data = HashMap<String, String>()
+        data["key"] = "value"
+        collect.setCustomData(data)
+
+        Mockito.verify(client).getTemporaryStorage()
+        assertEquals(1, client.getTemporaryStorage().getCustomData().size)
+    }
+
+    @Test
+    fun testResetCustomData() {
+        val client = applyApiClient()
+        Mockito.doReturn(VgsApiTemporaryStorageImpl())
+            .`when`(client).getTemporaryStorage()
+
+        collect.resetCustomData()
+
+        Mockito.verify(client).getTemporaryStorage()
+        assertEquals(0, client.getTemporaryStorage().getCustomData().size)
+    }
+
+    @Test
+    fun test_get_file_provider() {
+        val storage = applyStorage()
+        collect.getFileProvider()
+        Mockito.verify(storage).getVGSContentProvider()
+    }
+
+    private fun <T> any(): T = Mockito.any<T>()
+
+    private fun applyStorage(): InternalStorage {
+        val storage = spy( InternalStorage(activity) )
+        collect.setStorage(storage)
+
+        return storage
+    }
+
+    private fun applyEditText(typeField: FieldType):VGSEditText {
+        val view = spy( VGSEditText(activity) )
+        view.setFieldType(typeField)
+
+        collect.bindView(view)
+
+        return view
+    }
+
+    private fun applyResponseListener(): VgsCollectResponseListener {
+        val listener = mock(VgsCollectResponseListener::class.java)
+        collect.addOnResponseListeners(listener)
+
+        return listener
+    }
+
+    private fun applyStateChangeListener(): OnFieldStateChangeListener {
+        val listener = mock(OnFieldStateChangeListener::class.java)
+        collect.addOnFieldStateChangeListener(listener)
+
+        return listener
+    }
+
+    private fun applyApiClient(): ApiClient {
+        val client = mock(ApiClient::class.java)
+        collect.setClient(client)
+
+        return client
     }
 }
