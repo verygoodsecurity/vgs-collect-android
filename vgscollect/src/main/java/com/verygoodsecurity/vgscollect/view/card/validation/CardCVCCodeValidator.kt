ed9a06e3349a67fdd8c8dfdf450e4ac85eb934d9
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-04-23 13:44:08 +0300

    fix 4xCVC validation

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardCVCCodeValidator.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/CardCVCCodeValidator.kt
@@ -1,9 +1,11 @@
 package com.verygoodsecurity.vgscollect.view.card.validation
 
 /** @suppress */
-class CardCVCCodeValidator:VGSValidator {
+class CardCVCCodeValidator(
+    private val length:Int = 3
+):VGSValidator {
     override fun isValid(content: String?): Boolean {
         val data:Int? =  content?.trim()?.toIntOrNull()
-        return data != null && content.length >= 3
+        return data != null && content.length >= length
     }
 }
