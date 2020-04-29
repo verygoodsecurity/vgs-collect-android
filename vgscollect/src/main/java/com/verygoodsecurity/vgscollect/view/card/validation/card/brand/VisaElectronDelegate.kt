739cb5ad134287b672a939736004fcfa0c6d5636
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-04-29 17:33:17 +0300

    add Luhn for visa electron

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/VisaElectronDelegate.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/VisaElectronDelegate.kt
@@ -2,8 +2,7 @@ package com.verygoodsecurity.vgscollect.view.card.validation.card.brand
 
 import com.verygoodsecurity.vgscollect.view.card.validation.VGSValidator
 
-/** @suppress */
-class JcbDelegate : VGSValidator by DefaultCardValidator("^(?:2131|1800|35\\d{3})\\d{11,14}\$") {
+class VisaElectronDelegate : VGSValidator by DefaultCardValidator() {
     override fun isValid(content: String?): Boolean {
         return !content.isNullOrEmpty() && isLuhnChecksumValid(content)
     }
