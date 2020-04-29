f81de272bc28183013e1fdb70812fc67e1bd8e73
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-04-29 17:39:56 +0300

    update JavaDocs

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/VisaElectronDelegate.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/VisaElectronDelegate.kt
@@ -2,7 +2,8 @@ package com.verygoodsecurity.vgscollect.view.card.validation.card.brand
 
 import com.verygoodsecurity.vgscollect.view.card.validation.VGSValidator
 
-class VisaElectronDelegate : VGSValidator by DefaultCardValidator() {
+/** @suppress */
+class VisaElectronDelegate : VGSValidator by DefaultCardValidator("^4(026|17500|405|508|844|91[37])") {
     override fun isValid(content: String?): Boolean {
         return !content.isNullOrEmpty() && isLuhnCheckSumValid(content)
     }
