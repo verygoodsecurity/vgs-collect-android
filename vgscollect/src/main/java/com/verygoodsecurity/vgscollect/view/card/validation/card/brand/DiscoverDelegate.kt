bdee3605a4352fc9045b4af84fecf3919c5daae0
dmytro.kosolapenko <dmitriy.kosolapenko@vgs.io>
2020-04-29 17:37:45 +0300

    rename method

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/DiscoverDelegate.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/DiscoverDelegate.kt
@@ -5,6 +5,6 @@ import com.verygoodsecurity.vgscollect.view.card.validation.VGSValidator
 /** @suppress */
 class DiscoverDelegate : VGSValidator by DefaultCardValidator("^6(?:011|5[0-9]{2})[0-9]{12}\$") {
     override fun isValid(content: String?): Boolean {
-        return !content.isNullOrEmpty() && isLuhnChecksumValid(content)
+        return !content.isNullOrEmpty() && isLuhnCheckSumValid(content)
     }
 }
--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/JcbDelegate.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/card/brand/JcbDelegate.kt
@@ -5,6 +5,6 @@ import com.verygoodsecurity.vgscollect.view.card.validation.VGSValidator
 /** @suppress */
 class JcbDelegate : VGSValidator by DefaultCardValidator("^(?:2131|1800|35\\d{3})\\d{11,14}\$") {
     override fun isValid(content: String?): Boolean {
-        return !content.isNullOrEmpty() && isLuhnChecksumValid(content)
+        return !content.isNullOrEmpty() && isLuhnCheckSumValid(content)
     }
 }
