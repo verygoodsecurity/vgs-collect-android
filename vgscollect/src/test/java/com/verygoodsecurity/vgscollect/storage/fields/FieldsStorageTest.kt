6d2804b326fe48c1f253f2fc46347d57a7e6738d
Dmytro Kos <dmitriy.kosolapenko@gmail.com>
2020-05-15 16:57:09 +0300

    add tests

--- vgscollect/src/test/java/com/verygoodsecurity/vgscollect/storage/fields/FieldsStorageTest.kt
+++ vgscollect/src/test/java/com/verygoodsecurity/vgscollect/storage/fields/FieldsStorageTest.kt
@@ -3,44 +3,50 @@ package com.verygoodsecurity.vgscollect.storage.fields
 import com.verygoodsecurity.vgscollect.core.model.state.FieldContent
 import com.verygoodsecurity.vgscollect.core.model.state.FieldState
 import com.verygoodsecurity.vgscollect.core.model.state.VGSFieldState
-import com.verygoodsecurity.vgscollect.core.storage.*
+import com.verygoodsecurity.vgscollect.core.storage.FieldDependencyObserver
+import com.verygoodsecurity.vgscollect.core.storage.OnFieldStateChangeListener
+import com.verygoodsecurity.vgscollect.core.storage.StorageContractor
 import com.verygoodsecurity.vgscollect.core.storage.content.field.TemporaryFieldsStorage
 import com.verygoodsecurity.vgscollect.view.card.CardType
 import com.verygoodsecurity.vgscollect.view.card.FieldType
 import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
 
 class FieldsStorageTest {
 
-    private fun <T> any(): T = Mockito.any<T>()
+    private lateinit var store:TemporaryFieldsStorage
+
+    @Before
+    fun setupFieldStorage() {
+        val contractor:StorageContractor<VGSFieldState> = mock(StorageContractor::class.java) as StorageContractor<VGSFieldState>
+        Mockito.doReturn(true).`when`(contractor).checkState(any())
+        store = TemporaryFieldsStorage(contractor)
+    }
 
     @Test
     fun test_add_item() {
-        val store =
-            TemporaryFieldsStorage()
 
-        store.addItem(0, VGSFieldState(isFocusable = false))
+        store.addItem(0, VGSFieldState(isFocusable = false, fieldName = "1"))
         assertEquals(1, store.getItems().size)
 
-        store.addItem(1, VGSFieldState(isFocusable = false))
+        store.addItem(1, VGSFieldState(isFocusable = false, fieldName = "12"))
         assertEquals(2, store.getItems().size)
 
-        store.addItem(0, VGSFieldState(isFocusable = true))
+        store.addItem(0, VGSFieldState(isFocusable = true, fieldName = "3"))
         assertEquals(2, store.getItems().size)
 
-        store.addItem(2, VGSFieldState(isFocusable = true))
+        store.addItem(2, VGSFieldState(isFocusable = true, fieldName = "4"))
         assertEquals(3, store.getItems().size)
     }
 
     @Test
     fun test_clear() {
-        val store =
-            TemporaryFieldsStorage()
-        store.addItem(0, VGSFieldState())
-        store.addItem(1, VGSFieldState())
+        store.addItem(0, VGSFieldState(fieldName = "n1"))
+        store.addItem(1, VGSFieldState(fieldName = "n12"))
 
         assertEquals(2, store.getItems().size)
 
@@ -50,30 +56,23 @@ class FieldsStorageTest {
 
     @Test
     fun test_get_items() {
-        val store =
-            TemporaryFieldsStorage()
-
-        store.addItem(0, VGSFieldState(isFocusable = false))
-        store.addItem(1, VGSFieldState(isFocusable = true))
+        store.addItem(0, VGSFieldState(isFocusable = false, fieldName = "n1"))
+        store.addItem(1, VGSFieldState(isFocusable = true, fieldName = "n12"))
 
         assertEquals(2, store.getItems().size)
     }
 
     @Test
     fun test_perform_subscription() {
-        val store =
-            TemporaryFieldsStorage()
         val listener = store.performSubscription()
 
-        val item1 = VGSFieldState()
-        item1.fieldName = "1"
+        val item1 = VGSFieldState(fieldName = "n12")
         listener.emit(0, item1)
 
         val itemsCase1 = store.getItems()
         assertEquals(1, itemsCase1.size)
 
-        val item2 = VGSFieldState()
-        item2.fieldName = "2"
+        val item2 = VGSFieldState(fieldName = "n1")
         listener.emit(1, item2)
 
         val itemsCase2 = store.getItems()
@@ -83,8 +82,6 @@ class FieldsStorageTest {
     @Test
     fun test_attach_state_change_listener() {
         val listener = mock(OnFieldStateChangeListener::class.java)
-        val store =
-            TemporaryFieldsStorage()
         store.attachStateChangeListener(listener)
 
         assertEquals(1, store.getFieldStateChangeListeners().size)
@@ -93,11 +90,12 @@ class FieldsStorageTest {
     @Test
     fun test_trigger_state_change_listener() {
         val listener = mock(OnFieldStateChangeListener::class.java)
-        val store =
-            TemporaryFieldsStorage()
         store.attachStateChangeListener(listener)
 
-        store.addItem(0, VGSFieldState())
+        store.addItem(1, VGSFieldState(fieldName = "n13"))
+        Mockito.verify(listener, Mockito.times(0)).onStateChange(any())
+
+        store.addItem(1, VGSFieldState(fieldName = "n13", hasUserInteraction = true))
 
         Mockito.verify(listener).onStateChange(any())
     }
@@ -105,8 +103,6 @@ class FieldsStorageTest {
     @Test
     fun test_attach_field_dependency_observer() {
         val listener = mock(FieldDependencyObserver::class.java)
-        val store =
-            TemporaryFieldsStorage()
         store.attachFieldDependencyObserver(listener)
 
         assertEquals(1, store.getDependencyObservers().size)
@@ -114,29 +110,24 @@ class FieldsStorageTest {
 
     @Test
     fun test_trigger_field_dependency_observer_refresh_state() {
-        val store =
-            TemporaryFieldsStorage()
 
         val onFieldStateChangeListener = mock(FieldDependencyObserver::class.java)
         store.attachFieldDependencyObserver(onFieldStateChangeListener)
 
         val onVgsViewStateChangeListener = store.performSubscription()
-        onVgsViewStateChangeListener.emit(2, VGSFieldState(type = FieldType.CARD_NUMBER))
-        onVgsViewStateChangeListener.emit(1, VGSFieldState(type = FieldType.CVC))
+        onVgsViewStateChangeListener.emit(2, VGSFieldState(type = FieldType.CARD_NUMBER, fieldName = "n13"))
+        onVgsViewStateChangeListener.emit(1, VGSFieldState(type = FieldType.CVC, fieldName = "n1"))
 
         Mockito.verify(onFieldStateChangeListener).onRefreshState(any())
     }
 
     @Test
     fun test_trigger_field_dependency_observer_state_update() {
-        val store =
-            TemporaryFieldsStorage()
-
         val onFieldStateChangeListener = mock(FieldDependencyObserver::class.java)
         store.attachFieldDependencyObserver(onFieldStateChangeListener)
 
         val onVgsViewStateChangeListener = store.performSubscription()
-        onVgsViewStateChangeListener.emit(2, VGSFieldState(type = FieldType.CARD_NUMBER))
+        onVgsViewStateChangeListener.emit(2, VGSFieldState(type = FieldType.CARD_NUMBER, fieldName = "n"))
 
         Mockito.verify(onFieldStateChangeListener).onStateUpdate(any())
     }
@@ -150,14 +141,14 @@ class FieldsStorageTest {
             }
         }
 
-        val store =
-            TemporaryFieldsStorage()
         store.attachStateChangeListener(listener)
 
         store.addItem(0, VGSFieldState(isFocusable = false, isRequired = true, fieldName = "alias"))
+
+        store.addItem(0, VGSFieldState(isFocusable = false, isRequired = true, fieldName = "alias", hasUserInteraction = true))
         Assert.assertNotNull("FieldState didn't update", userLastUpdatedState)
 
-        val viewState = VGSFieldState(isFocusable = true, isRequired = false, fieldName = "alias1")
+        val viewState = VGSFieldState(isFocusable = true, isRequired = false, fieldName = "alias")
         store.addItem(0, viewState)
 
         val isEqual = userLastUpdatedState?.hasFocus == viewState.isFocusable &&
@@ -169,12 +160,10 @@ class FieldsStorageTest {
     @Test
     fun test_CVC_3_Length_dependency_detector() {
         val observer = mock(FieldDependencyObserver::class.java)
-        val store =
-            TemporaryFieldsStorage()
         store.attachFieldDependencyObserver(observer)
 
         val listener = store.performSubscription()
-        listener.emit(0, VGSFieldState(isFocusable = false, type = FieldType.CVC))
+        listener.emit(0, VGSFieldState(isFocusable = false, type = FieldType.CVC, fieldName = "n12"))
 
         val state = createMASTERCARD()
         val content = FieldContent.CardNumberContent()
@@ -185,11 +174,14 @@ class FieldsStorageTest {
     }
 
     private fun createMASTERCARD() : VGSFieldState {
-        val state = VGSFieldState(isFocusable = false, type = FieldType.CARD_NUMBER )
+        val state = VGSFieldState(isFocusable = false, type = FieldType.CARD_NUMBER, fieldName = "n12" )
         val content = FieldContent.CardNumberContent()
         content.cardtype = CardType.MASTERCARD
         state.content = content
 
         return state
     }
+
+    private fun <T> any(): T = Mockito.any<T>()
+
 }
