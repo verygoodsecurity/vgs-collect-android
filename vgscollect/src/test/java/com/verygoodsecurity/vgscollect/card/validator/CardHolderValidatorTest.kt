402a16e76bf5be1e234ac25a46cc34f210bc5c36
Dmytro Kos <dmitriy.kosolapenko@gmail.com>
2020-06-30 14:25:54 +0300

    update tests

--- vgscollect/src/test/java/com/verygoodsecurity/vgscollect/card/validator/CardHolderValidatorTest.kt
+++ vgscollect/src/test/java/com/verygoodsecurity/vgscollect/card/validator/CardHolderValidatorTest.kt
@@ -3,10 +3,16 @@ package com.verygoodsecurity.vgscollect.card.validator
 import com.verygoodsecurity.vgscollect.view.card.validation.CardHolderValidator
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class CardHolderValidatorTest {
-    private val validator = CardHolderValidator()
+    private lateinit var validator:CardHolderValidator
+
+    @Before
+    fun setupValidator() {
+        validator = CardHolderValidator("^[a-zA-Z0-9 ,'.-]+\$")
+    }
 
     @Test
     fun name_with_special_symbols() {
@@ -21,6 +27,30 @@ class CardHolderValidatorTest {
     @Test
     fun name_dafault() {
         assertTrue(validator.isValid("abra cadabra"))
+        assertTrue(validator.isValid("abra cadab ra"))
+    }
+
+    @Test
+    fun test_custom_regex() {
+        validator.setRegex("^([a-zA-Z]{2,}\\s[a-zA-z]{1,})\$")
+
+        assertFalse(validator.isValid("abra"))
+        assertTrue(validator.isValid("abra cadabra"))
+        assertFalse(validator.isValid("abra cadab ra"))
+        assertFalse(validator.isValid("qq aa$"))
+    }
+
+    @Test
+    fun testEmptyValidator() {
+        val validator = CardHolderValidator()
+
+        assertTrue(validator.isValid(""))
+        assertTrue(validator.isValid(null))
+        assertTrue(validator.isValid("abra"))
+        assertTrue(validator.isValid("abra cadabra"))
+        assertTrue(validator.isValid("abra cadab ra"))
+        assertTrue(validator.isValid("qq aa$"))
+        assertTrue(validator.isValid("( * 2q aa$"))
     }
 
 }
