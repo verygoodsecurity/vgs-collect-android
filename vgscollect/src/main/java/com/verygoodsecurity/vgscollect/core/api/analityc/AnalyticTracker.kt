808aa59b346b814e1d45fc676f5179ce5b987747
Dmytro Kos <dmitriy.kosolapenko@vgs.io>
2020-12-22 18:43:30 +0300

    Release 1.3.2 (#62)
    * increase version
    
    * Fixed hintTextColor attribute
    
    * Update CI script (#56)
    
    * prevent hostname validation if the base URL contains mistakes (#57)
    
    * Update dependencies (#58)
    
    * Added ability to disable/enable tracking analytic events (#59)
    
    * generate docs (#60)
    
    * Release 1.3.2 (#61)

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/api/analityc/AnalyticTracker.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/api/analityc/AnalyticTracker.kt
@@ -3,5 +3,6 @@ package com.verygoodsecurity.vgscollect.core.api.analityc
 import com.verygoodsecurity.vgscollect.core.api.analityc.action.Action
 
 interface AnalyticTracker {
+    fun setAnalyticsEnabled(isEnabled: Boolean)
     fun logEvent(action: Action)
 }
