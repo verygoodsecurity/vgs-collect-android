1770c097243e5a7d7ca3720e870122307c664699
Dmytro <dmitriy.kosolapenko@vgs.io>
2021-01-11 20:07:55 +0200

    update bouncer scan module version

--- vgscollect-bouncer/src/main/java/com/verygoodsecurity/api/bouncer/ScanActivity.kt
+++ vgscollect-bouncer/src/main/java/com/verygoodsecurity/api/bouncer/ScanActivity.kt
@@ -13,19 +13,19 @@ import com.verygoodsecurity.vgscollect.app.BaseTransmitActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
-class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
+class ScanActivity : BaseTransmitActivity(), CardScanActivityResultHandler {
 
-    private lateinit var key:String
+    private lateinit var key: String
 
-    private lateinit var settings:Map<String, Int>
+    private lateinit var settings: Map<String, Int>
 
     private var enableEnterCardManually = false
     private var enableExpiryExtraction = false
     private var enableNameExtraction = false
     private var displayCardPan = false
     private var displayCardholderName = false
-    private  var displayCardScanLogo = false
-    private  var enableDebug = false
+    private var displayCardScanLogo = false
+    private var enableDebug = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +50,7 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
         intent.extras?.let {
             settings = it.getSerializable(SCAN_CONFIGURATION)?.run {
                 this as HashMap<String, Int>
-            }?:HashMap()
+            } ?: HashMap()
             key = it.getString(API_KEY, "")
 
             enableExpiryExtraction = it.getBoolean(ENABLE_EXPIRY_EXTRACTION)
@@ -63,7 +63,8 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
     }
 
     private fun runCardIO() {
-        CardScanActivity.start(this,
+        CardScanActivity.start(
+            this,
             key,
             enableEnterCardManually,
             enableExpiryExtraction,
@@ -79,53 +80,67 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
 
     override fun analyzerFailure(scanId: String?) {}
 
-    override fun cameraError(scanId: String?) {}
+    override fun cameraError(scanId: String?) {
+        addAnalyticInfo(scanId, Status.FAILED)
+        setResult(Activity.RESULT_OK, resultIntent)
+    }
 
-    override fun canceledUnknown(scanId: String?) {}
+    override fun canceledUnknown(scanId: String?) {
+        addAnalyticInfo(scanId, Status.FAILED)
+        setResult(Activity.RESULT_OK, resultIntent)
+    }
+
+    override fun enterManually(scanId: String?) {}
+
+    override fun userCanceled(scanId: String?) {
+        addAnalyticInfo(scanId, Status.CLOSE)
+        setResult(Activity.RESULT_OK, resultIntent)
+    }
 
     override fun cardScanned(scanId: String?, scanResult: CardScanActivityResult) {
         settings.forEach {
-            when(it.value) {
+            when (it.value) {
                 CARD_NUMBER -> mapData(it.key, scanResult.pan)
                 CARD_CVC -> mapData(it.key, scanResult.cvc)
                 CARD_HOLDER -> mapData(it.key, scanResult.cardholderName)
                 CARD_EXP_DATE -> mapData(it.key, retrieveDate(scanResult))
             }
         }
+
+        addAnalyticInfo(scanId, Status.SUCCESS)
     }
 
     private fun retrieveDate(scanResult: CardScanActivityResult): Long? {
-        val day:Int
-        val mounts:Int
-        val year:Int
+        val day: Int
+        val mounts: Int
+        val year: Int
 
         scanResult.also {
-            day = it.expiryDay?.toIntOrNull()?:0
+            day = it.expiryDay?.toIntOrNull() ?: 0
         }.also {
-            mounts = it.expiryMonth?.toIntOrNull()?:0
+            mounts = it.expiryMonth?.toIntOrNull() ?: 0
         }.also {
-            year = it.expiryYear?.toIntOrNull()?:0
+            year = it.expiryYear?.toIntOrNull() ?: 0
         }
 
-        return if(mounts != 0 && year != 0) {
-            val dStr:String
-            val dMask:String
-            if(day != 0) {
-                dStr = String.format("%02d", day)+"/"
-                dMask = day.toString().replace("\\d".toRegex(), "d")+"/"
+        return if (mounts != 0 && year != 0) {
+            val dStr: String
+            val dMask: String
+            if (day != 0) {
+                dStr = String.format("%02d", day) + "/"
+                dMask = day.toString().replace("\\d".toRegex(), "d") + "/"
             } else {
                 dStr = ""
                 dMask = ""
             }
 
-
             val mMask = String.format("%02d", mounts)
-                .replace("\\d".toRegex(), "M")+"/"
+                .replace("\\d".toRegex(), "M") + "/"
 
             val yMask = mounts.toString()
                 .replace("\\d".toRegex(), "y")
 
-            val mStr = String.format("%02d", mounts)+"/"
+            val mStr = String.format("%02d", mounts) + "/"
             val yStr = year.toString()
             val date = SimpleDateFormat("$dMask$mMask$yMask", Locale.getDefault())
                 .parse("$dStr$mStr$yStr")
@@ -135,12 +150,15 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
         }
     }
 
-    override fun enterManually(scanId: String?) {}
-
-    override fun userCanceled(scanId: String?) {}
+    private fun addAnalyticInfo(scanId: String?, status: Status) {
+        mapData(RESULT_TYPE, SCAN)
+        mapData(RESULT_NAME, NAME)
+        mapData(RESULT_ID, scanId)
+        mapData(RESULT_STATUS, status.raw)
+    }
 
     companion object {
-        private const val NAME = "ScanActivity"
+        private const val NAME = "Bouncer"
         const val SCAN_CONFIGURATION = "vgs_scan_settings"
 
         const val CARD_NUMBER = 0x71
@@ -186,7 +204,7 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
         /**
         Start the card scanner activity.
          */
-        fun scan(context:Activity, code:Int, bndl:Bundle = Bundle.EMPTY) {
+        fun scan(context: Activity, code: Int, bndl: Bundle = Bundle.EMPTY) {
             val intent = Intent(context, ScanActivity::class.java)
             intent.putExtras(bndl)
             context.startActivityForResult(intent, code)
@@ -195,7 +213,7 @@ class ScanActivity: BaseTransmitActivity(), CardScanActivityResultHandler {
 
     private fun printLog(e: Exception) {
         if (BuildConfig.DEBUG) {
-            Log.e(NAME, e.message?:"", e)
+            Log.e(NAME, e.message ?: "", e)
         }
     }
 }
