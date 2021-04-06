ab90af28ad49fb21a5885c997be65d2d49d68793
Dmytro <dmitriy.kosolapenko@vgs.io>
2021-04-06 11:27:06 +0300

    add validation rule for VGSEditText

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/rules/PaymentCardNumberRule.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/rules/PaymentCardNumberRule.kt
@@ -104,24 +104,4 @@ class PaymentCardNumberRule private constructor(
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PaymentCardNumberRule
-
-        if (algorithm != other.algorithm) return false
-        if (length != null) {
-            if (other.length == null) return false
-            if (!length.contentEquals(other.length)) return false
-        } else if (other.length != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = algorithm?.hashCode() ?: 0
-        result = 31 * result + (length?.contentHashCode() ?: 0)
-        return result
-    }
 }
