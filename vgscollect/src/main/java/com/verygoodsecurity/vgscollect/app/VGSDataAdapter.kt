67e0a067ec23ed3ad27063cf478a6d3f0ca1358f
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-19 15:33:07 +0300

    Remove redundant code

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/app/VGSDataAdapter.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/app/VGSDataAdapter.kt
@@ -3,22 +3,11 @@ package com.verygoodsecurity.vgscollect.app
 import com.verygoodsecurity.vgscollect.core.VGSCollect
 import com.verygoodsecurity.vgscollect.core.model.VGSHashMapWrapper
 
-private const val NFC_ADAPTER_CLASSNAME = "VGSNFCAdapter"
-
 abstract class VGSDataAdapter {
 
     internal var collect: VGSCollect? = null
 
     protected fun setData(data: Map<String, Any?>) {
-        checkIsValidInheritance {
-            collect?.dispatchData(VGSHashMapWrapper(HashMap(data)))
-        }
-    }
-
-    private fun checkIsValidInheritance(action: () -> Unit) {
-        when (val name = this::class.java.simpleName) {
-            NFC_ADAPTER_CLASSNAME -> action.invoke()
-            else -> throw IllegalArgumentException("This class($name) should not inherit from VGSDataAdapter!")
-        }
+        collect?.dispatchData(VGSHashMapWrapper(HashMap(data)))
     }
 }
