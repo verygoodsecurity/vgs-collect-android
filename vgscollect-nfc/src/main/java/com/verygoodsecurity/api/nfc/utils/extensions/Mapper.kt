c2a5c6f02042f33d6e73d2016938136acd99af66
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-04-20 10:49:08 +0300

    Add support of VGSDataMapper.kt in card scan modules

--- vgscollect-nfc/src/main/java/com/verygoodsecurity/api/nfc/utils/extensions/Mapper.kt
+++ vgscollect-nfc/src/main/java/com/verygoodsecurity/api/nfc/utils/extensions/Mapper.kt
@@ -3,5 +3,5 @@ package com.verygoodsecurity.api.nfc.utils.extensions
 import com.verygoodsecurity.api.nfc.core.model.Card
 import com.verygoodsecurity.vgscollect.app.mapper.VGSCard
 
-// TODO: Add holderName & cvc
-internal fun Card.toVGSCard() = VGSCard(number, "", date, "")
+// TODO: Add holderName && cvc && postal code
+internal fun Card.toVGSCard() = VGSCard(number, null, date, null, null)
--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/app/mapper/VGSDataMapper.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/app/mapper/VGSDataMapper.kt
@@ -1,23 +1,26 @@
 package com.verygoodsecurity.vgscollect.app.mapper
 
 import android.os.Parcelable
+import com.verygoodsecurity.vgscollect.util.extension.putIfNotNull
 import kotlinx.parcelize.Parcelize
 
 /** Provides mapping between views and data. */
 @Parcelize
-class VGSDataMapper private constructor(
+class VGSDataMapper internal constructor(
     private val cardNumberFieldName: String?,
     private val cardHolderFieldName: String?,
     private val expirationDateFieldName: String?,
     private val cvcFieldName: String?,
-): Parcelable {
+    private val postalCodeFieldName: String?,
+) : Parcelable {
 
-    fun map(card: VGSCard): Map<String, String> {
-        val result = mutableMapOf<String, String>()
-        cardNumberFieldName?.let { result[it] = card.number }
-        cardHolderFieldName?.let { result[it] = card.holderName }
-        expirationDateFieldName?.let { result[it] = card.expirationDate }
-        cvcFieldName?.let { result[it] = card.cvc }
+    fun map(card: VGSCard): Map<String, Any?> {
+        val result = mutableMapOf<String, Any?>()
+        result.putIfNotNull(cardNumberFieldName, card.number)
+        result.putIfNotNull(cardHolderFieldName, card.holderName)
+        result.putIfNotNull(expirationDateFieldName, card.expirationDate)
+        result.putIfNotNull(cvcFieldName, card.cvc)
+        result.putIfNotNull(postalCodeFieldName, card.postalCode)
         return result
     }
 
@@ -28,6 +31,7 @@ class VGSDataMapper private constructor(
         private var cardHolderFieldName: String? = null
         private var expirationDateFieldName: String? = null
         private var cvcFieldName: String? = null
+        private var postalCodeFieldName: String? = null
 
         /**
          * Specify field name of card number view that should be updated.
@@ -57,12 +61,20 @@ class VGSDataMapper private constructor(
             cvcFieldName = fieldName
         }
 
+        /**
+         * Specify field name of postal code view that should be updated.
+         */
+        fun setPostalCode(fieldName: String?) = this.apply {
+            postalCodeFieldName = fieldName
+        }
+
         /** Create instance of [VGSDataMapper] */
         fun build() = VGSDataMapper(
             cardNumberFieldName,
             cardHolderFieldName,
             expirationDateFieldName,
-            cvcFieldName
+            cvcFieldName,
+            postalCodeFieldName
         )
     }
 }
