56e61b5079e7c6051cbd4d253020019c78af3c68
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-05-30 12:18:33 +0300

    Remove logs

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/InputStream.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/InputStream.kt
@@ -1,6 +1,5 @@
 package com.verygoodsecurity.vgscollect.util.extension
 
-import android.util.Log
 import java.io.InputStream
 import kotlin.math.min
 
@@ -8,10 +7,7 @@ internal class NotEnoughMemoryException : Exception()
 
 @Throws(NotEnoughMemoryException::class)
 internal inline fun <R> InputStream.useIfMemoryEnough(maxSize: Long, block: (InputStream) -> R): R {
-    val halfOfFreeMemory = Runtime.getRuntime().freeMemory() / 2
-    val target = min(halfOfFreeMemory, maxSize)
-    Log.d("Test", "useIfMemoryEnough, maxSize = ${maxSize / 1024 / 1024}, halfOfFreeMemory = ${halfOfFreeMemory / 1024 / 1024}, target = ${target / 1024 / 1024}")
-    if (this.available() >= target) {
+    if (this.available() >= min(Runtime.getRuntime().freeMemory() / 2, maxSize)) {
         throw NotEnoughMemoryException()
     }
     this.use {
