2471f9b2b34c37a7e0ea08f4353d08124b505a38
Dmytro <dmitriy.kosolapenko@vgs.io>
2021-09-16 13:58:41 +0300

    add default validation to VGSEdittext

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/InfoValidator.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/InfoValidator.kt
@@ -1,14 +1,9 @@
 package com.verygoodsecurity.vgscollect.view.card.validation
 
-import java.util.regex.Pattern
-
 /** @suppress */
-class InfoValidator:VGSValidator {
-    private val m = Pattern.compile(".*?")
+class InfoValidator : VGSValidator {
 
     override fun isValid(content: String?): Boolean {
-        val str = content?.trim()
-        return !str.isNullOrEmpty() &&
-                m.matcher(str).matches()
+        return !content?.trim().isNullOrEmpty()
     }
 }
