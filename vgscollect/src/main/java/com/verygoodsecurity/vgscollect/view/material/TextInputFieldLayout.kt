dceafce80ed76f9c1102b14cc6ed1552fc4c30e3
DmytroDm <72735363+DmytroDm@users.noreply.github.com>
2021-12-15 18:50:21 +0200

    Fix TextInputFieldLayout.kt error restore after screen rotation (#312)

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/material/TextInputFieldLayout.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/material/TextInputFieldLayout.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Typeface
 import android.os.Build
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,7 @@ import androidx.annotation.RequiresApi
 import androidx.annotation.StyleRes
 import androidx.annotation.VisibleForTesting
 import com.verygoodsecurity.vgscollect.view.InputFieldView
+import com.verygoodsecurity.vgscollect.view.material.internal.TextInputLayoutSavedState
 import com.verygoodsecurity.vgscollect.view.material.internal.InputLayoutStateImpl
 import com.verygoodsecurity.vgscollect.view.material.internal.TextInputLayoutWrapper
 import com.verygoodsecurity.vgscollect.widget.VGSTextInputLayout
@@ -40,6 +42,21 @@ abstract class TextInputFieldLayout @JvmOverloads constructor(
         fieldState = InputLayoutStateImpl(textInputLayout)
 
         addView(textInputLayout)
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        return TextInputLayoutSavedState(super.onSaveInstanceState()).apply {
+            this.error = fieldState.error?.toString()
+        }
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if ((state as? TextInputLayoutSavedState) != null) {
+            super.onRestoreInstanceState(state.superState)
+            fieldState.error = state.error
+        } else {
+            super.onRestoreInstanceState(state)
+        }
     }
 
     /**
