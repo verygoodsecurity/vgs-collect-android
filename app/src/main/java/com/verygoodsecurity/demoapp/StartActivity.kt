1dfef3c07e470116a915321cab52c578b45c174a
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2022-07-07 18:30:34 +0300

    Update environment selection handling

--- app/src/main/java/com/verygoodsecurity/demoapp/StartActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoapp/StartActivity.kt
@@ -3,100 +3,58 @@ package com.verygoodsecurity.demoapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.verygoodsecurity.demoapp.activity_case.VGSCollectActivity
 import com.verygoodsecurity.demoapp.fragment_case.VGSCollectFragmentActivity
+import com.verygoodsecurity.demoapp.tokenization.VGSCollectTokenizationActivity
 import com.verygoodsecurity.demoapp.viewpager_case.VGSViewPagerActivity
-import com.verygoodsecurity.vgscollect.core.Environment
 import kotlinx.android.synthetic.main.activity_start.*
 
-class StartActivity:AppCompatActivity(), View.OnClickListener {
-
-    companion object {
-        const val VAULT_ID = "user_vault_id"
-        const val ENVIROMENT = "user_env"
-        const val PATH = "user_path"
-    }
-
-    private val spinnerAdapter:ArrayAdapter<String> by lazy {
-        val envArr = arrayOf(
-            Environment.SANDBOX.rawValue.toUpperCase(),
-            Environment.LIVE.rawValue.toUpperCase()
-        )
-        val layout = android.R.layout.simple_spinner_item
-        val spinnerArrayAdapter = ArrayAdapter<String>(this, layout, envArr)
-
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        spinnerArrayAdapter
-    }
+class StartActivity : AppCompatActivity(R.layout.activity_start) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-
-        setupSpinner()
-        setupUI()
-
-        startTokenizationBtn?.setOnClickListener(this)
-        startWithActivityBtn?.setOnClickListener(this)
-        startWithFragmentBtn?.setOnClickListener(this)
-        startWithViewPagerBtn?.setOnClickListener(this)
+        setupViews()
     }
 
-    private fun setupUI() {
-        userVault?.setText(BuildConfig.VAULT_ID)
-        userPath?.setText(BuildConfig.PATH)
-
-        environmentSpinner.setSelection(BuildConfig.ENVIRINMENT.ordinal)
-    }
-
-    private fun setupSpinner() {
-        environmentSpinner.adapter = spinnerAdapter
-    }
-
-    override fun onClick(v: View) {
-        when(v.id) {
-            R.id.startTokenizationBtn -> startTokenization()
-            R.id.startWithActivityBtn -> startInteractionWithActivity()
-            R.id.startWithFragmentBtn -> startInteractionWithFragment()
-            R.id.startWithViewPagerBtn -> startInteractionWithViewPager()
+    private fun setupViews() {
+        tiedVaultId?.setText(BuildConfig.VAULT_ID)
+        tiedPath?.setText(BuildConfig.PATH)
+        llTokenizationFlow?.setOnClickListener {
+            startActivity(VGSCollectTokenizationActivity::class.java)
+        }
+        llCollectActivityFlow?.setOnClickListener {
+            startActivity(VGSCollectActivity::class.java)
+        }
+        llCollectFragmentFlow?.setOnClickListener {
+            startActivity(VGSCollectFragmentActivity::class.java)
+        }
+        llCollectViewPagerFlow?.setOnClickListener {
+            startActivity(VGSViewPagerActivity::class.java)
         }
     }
 
-
-    private fun startTokenization() {
-        // TODO: Implement
-        val intent = prepareIntent(VGSCollectActivity::class.java)
-        startActivity(intent)
+    private fun startActivity(activity: Class<out Activity>) {
+        startActivity(Intent(this, activity).apply {
+            putExtra(KEY_BUNDLE_VAULT_ID, tiedVaultId.text.toString())
+            putExtra(KEY_BUNDLE_PATH, tiedPath.text.toString())
+            putExtra(KEY_BUNDLE_ENVIRONMENT, getEnvironment())
+        })
     }
 
-    private fun startInteractionWithActivity() {
-        val intent = prepareIntent(VGSCollectActivity::class.java)
-        startActivity(intent)
+    private fun getEnvironment() = when (mbGroupEnvironment.checkedButtonId) {
+        R.id.mbSandbox -> SANDBOX
+        R.id.mbLive -> LIVE
+        else -> throw IllegalArgumentException("Not implemented")
     }
 
-    private fun startInteractionWithFragment() {
-        val intent = prepareIntent(VGSCollectFragmentActivity::class.java)
-        startActivity(intent)
-    }
+    companion object {
 
-    private fun startInteractionWithViewPager() {
-        val intent = prepareIntent(VGSViewPagerActivity::class.java)
-        startActivity(intent)
-    }
+        private const val SANDBOX = "sandbox"
+        private const val LIVE = "live"
 
-    private fun prepareIntent(componentClass: Class<out Activity>):Intent {
-        return Intent(this, componentClass).apply {
-            val vaultId = userVault.text.toString()
-            val path = userPath.text.toString()
-            val env = environmentSpinner.selectedItemPosition
-
-            putExtra(VAULT_ID, vaultId)
-            putExtra(ENVIROMENT, env)
-            putExtra(PATH, path)
-        }
+        const val KEY_BUNDLE_VAULT_ID = "user_vault_id"
+        const val KEY_BUNDLE_ENVIRONMENT = "user_env"
+        const val KEY_BUNDLE_PATH = "user_path"
     }
 }
