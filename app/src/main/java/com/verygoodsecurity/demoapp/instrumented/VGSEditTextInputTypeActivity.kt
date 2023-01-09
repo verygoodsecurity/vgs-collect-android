a6189c831affe65a1d1e086047b6cec7338cc3c0
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2023-01-09 15:44:24 +0200

    Remove kotlin synthetic from other places

--- app/src/main/java/com/verygoodsecurity/demoapp/instrumented/VGSEditTextInputTypeActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoapp/instrumented/VGSEditTextInputTypeActivity.kt
@@ -3,7 +3,6 @@ package com.verygoodsecurity.demoapp.instrumented
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.verygoodsecurity.demoapp.R
-import kotlinx.android.synthetic.main.instrumented_activity_vgs_edittext.*
 
 class VGSEditTextInputTypeActivity : AppCompatActivity() {
 
