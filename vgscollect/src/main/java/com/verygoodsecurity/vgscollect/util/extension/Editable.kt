025a3e405d1fb5f1b6628a045550bb1d326daf75
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2023-06-12 18:24:18 +0300

    Fix nullability

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/Editable.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/Editable.kt
@@ -2,10 +2,10 @@ package com.verygoodsecurity.vgscollect.util.extension
 
 import android.text.Editable
 
-internal fun Editable.replaceIgnoreFilters(start: Int, end: Int, text: CharSequence): Editable {
+internal fun Editable.replaceIgnoreFilters(start: Int, end: Int, text: CharSequence): Editable? {
     val f = filters
     filters = arrayOf()
     val result = replace(start, end, text)
-    result.filters = f
+    result?.filters = f
     return result
 }
