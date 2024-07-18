00e0df951c15fd3830e63b1eb9792ceb81bfd1eb
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2024-07-18 11:19:04 +0300

    Replace deprecated functions

---
+++ vgscollect-cardio/src/main/java/com/verygoodsecurity/api/cardio/extensions/Bundle.kt
@@ -0,0 +1,18 @@
+package com.verygoodsecurity.api.cardio.extensions
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+import java.io.Serializable
+
+inline fun <reified T : Serializable> Bundle.serializable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getSerializable(key) as? T
+}
+
+
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
