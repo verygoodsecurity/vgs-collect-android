79fdb19a3f58bc77726bd94474412985cef31b71
DmytroDm <dmytro.dmytryshyn@vgs.io>
2025-03-10 11:33:52 +0200

    Fix tests

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/VGSDate.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/core/model/VGSDate.kt
@@ -7,12 +7,12 @@ import java.util.*
  * Date representation that includes a day, month and year.
  */
 class VGSDate private constructor(
-    val day: Int?,
+    val day: Int,
     val month: Int,
     val year: Int
 ) {
     val timeInMillis: Long = Calendar.getInstance().apply {
-        set(Calendar.DAY_OF_MONTH, day ?: 1)
+        set(Calendar.DAY_OF_MONTH, day)
         set(Calendar.MONTH, month - 1)
         set(Calendar.YEAR, year)
         setMaximumTime()
@@ -20,10 +20,10 @@ class VGSDate private constructor(
 
     companion object {
 
-        fun create(day: Int?, month: Int, year: Int): VGSDate? {
+        fun create(day: Int, month: Int, year: Int): VGSDate? {
             // Create a calendar and set the components to create the date
             val calendar = Calendar.getInstance().apply {
-                set(Calendar.DAY_OF_MONTH, day ?: 1)
+                set(Calendar.DAY_OF_MONTH, day)
                 set(Calendar.MONTH, month - 1)
                 set(Calendar.YEAR, year)
                 setMaximumTime()
@@ -35,7 +35,7 @@ class VGSDate private constructor(
             val calYear = calendar.get(Calendar.YEAR)
 
             // If the calendar has the same values sent by parameter, the date is valid
-            if ((day == null || day == calDay) && month == (calMonth + 1) && year == calYear) {
+            if (day == calDay && month == (calMonth + 1) && year == calYear) {
                 return VGSDate(day, month, year)
             }
 
