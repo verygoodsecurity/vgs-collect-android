f7057d8e0067211aa70b871326ea3bce871cd941
DmytroDm <dmytro.dmytryshyn@vgs.io>
2025-07-02 18:47:13 +0300

    Add demo

---
+++ app/src/main/java/com/verygoodsecurity/demoapp/cmp/CMPActivity.kt
@@ -0,0 +1,116 @@
+package com.verygoodsecurity.demoapp.cmp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.verygoodsecurity.demoapp.R
+import com.verygoodsecurity.demoapp.databinding.CmpActivityBinding
+import com.verygoodsecurity.demoapp.databinding.CodeExampleLayoutBinding
+import com.verygoodsecurity.vgscollect.VGSCollectLogger
+import com.verygoodsecurity.vgscollect.core.Environment
+import com.verygoodsecurity.vgscollect.core.VGSCollect
+import com.verygoodsecurity.vgscollect.core.VgsCollectResponseListener
+import com.verygoodsecurity.vgscollect.core.model.network.VGSResponse
+import com.verygoodsecurity.vgscollect.view.core.serializers.VGSExpDateSeparateSerializer
+import io.github.kbiakov.codeview.adapters.Options
+import io.github.kbiakov.codeview.highlight.ColorThemeData
+import io.github.kbiakov.codeview.highlight.SyntaxColors
+import org.json.JSONObject
+
+class CMPActivity : AppCompatActivity(), VgsCollectResponseListener {
+
+    private lateinit var binding: CmpActivityBinding
+    private lateinit var codeExampleBinding: CodeExampleLayoutBinding
+
+    private val collect: VGSCollect by lazy {
+        VGSCollect.createCMP(
+            context = this@CMPActivity,
+            accountId = "test",
+            environment = Environment.SANDBOX
+        ).apply {
+            addOnResponseListeners(this@CMPActivity)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CmpActivityBinding.inflate(layoutInflater)
+        codeExampleBinding = CodeExampleLayoutBinding.bind(binding.root)
+        setContentView(binding.root)
+        VGSCollectLogger.logLevel = VGSCollectLogger.Level.DEBUG
+        initViews()
+    }
+
+    override fun onResponse(response: VGSResponse?) {
+        binding.progressBar.visibility = View.GONE
+        println(response)
+        updateCodeExample(response?.body)
+    }
+
+    private fun initViews() {
+        initPanView()
+        initCvcView()
+        initExpiryView()
+        initProceedView()
+        initCodeExampleView()
+        updateCodeExample(null)
+    }
+
+
+    private fun initPanView() {
+        collect.bindView(binding.vgsTiedPan)
+    }
+
+    private fun initCvcView() {
+        collect.bindView(binding.vgsTiedCvc)
+    }
+
+    private fun initExpiryView() {
+        binding.vgsTiedExpiry.setSerializer(
+            VGSExpDateSeparateSerializer(
+                monthFieldName = "exp_month",
+                yearFieldName = "exp_year"
+            )
+        )
+        collect.bindView(binding.vgsTiedExpiry)
+    }
+
+    private fun initProceedView() {
+        binding.mbProceed.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            collect.setCustomHeaders(mapOf("Authorization" to "Bearer <ACCESS_TOKEN>")) // Setup access token
+            collect.createCard()
+        }
+    }
+
+    private fun initCodeExampleView() {
+        val syntaxColor = ContextCompat.getColor(this, R.color.veryLightGray)
+        val bgColor = ContextCompat.getColor(this, R.color.blackPearl)
+        val lineNumberColor = ContextCompat.getColor(this, R.color.nobel)
+        codeExampleBinding.cvResponse.setOptions(
+            Options(
+                context = this.applicationContext, theme = ColorThemeData(
+                    SyntaxColors(
+                        string = syntaxColor,
+                        punctuation = syntaxColor,
+                    ),
+                    numColor = lineNumberColor,
+                    bgContent = bgColor,
+                    bgNum = bgColor,
+                    noteColor = syntaxColor,
+                )
+            )
+        )
+        codeExampleBinding.cvResponse.alpha = 1f
+    }
+
+    private fun updateCodeExample(response: String?) {
+        val json = try {
+            JSONObject(response ?: "").toString(4)
+        } catch (_: Exception) {
+            ""
+        }
+        codeExampleBinding.cvResponse.setCode(json)
+    }
+}
