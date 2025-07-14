128fdc9c55b9f6929667e5b51e57e0b1baca1fb6
DmytroDm <dmytro.dmytryshyn@vgs.io>
2025-07-14 17:46:36 +0300

    Implement custom brands, bin and last4

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/widget/compose/VgsCardNumberTextField.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/widget/compose/VgsCardNumberTextField.kt
@@ -21,6 +21,7 @@ import com.verygoodsecurity.vgscollect.widget.compose.mask.VgsMaskVisualTransfor
 import com.verygoodsecurity.vgscollect.widget.compose.validator.VgsRequiredFieldValidator
 import com.verygoodsecurity.vgscollect.widget.compose.validator.core.VgsTextFieldValidationResult
 import com.verygoodsecurity.vgscollect.widget.compose.validator.core.VgsTextFieldValidator
+import kotlin.math.max
 import kotlin.math.min
 
 class VgsCardNumberTextFieldState internal constructor(
@@ -28,30 +29,38 @@ class VgsCardNumberTextFieldState internal constructor(
     fieldName: String,
     validators: List<VgsTextFieldValidator>?,
     val isCardBrandValidationEnabled: Boolean,
+    val supportedCardBrands: List<VgsCardBrand>,
     val cardBrand: VgsCardBrand,
 ) : BaseFieldState(text, fieldName, validators) {
+
+    val bin: String? = generateBin()
+
+    val last4: String? = generateLast4()
 
     constructor(
         fieldName: String,
         validators: List<VgsTextFieldValidator>? = null,
         isCardBrandValidationEnabled: Boolean = true,
+        supportedCardBrands: List<VgsCardBrand> = VgsCardBrand.DEFAULT
     ) : this(
         EMPTY,
         fieldName,
         validators,
         isCardBrandValidationEnabled,
+        supportedCardBrands,
         VgsCardBrand.UNKNOWN,
     )
 
     override fun getOutputText(): String = text
 
     internal fun copy(text: String): VgsCardNumberTextFieldState {
-        val cardBrand = VgsCardBrand.detect(text)
+        val cardBrand = VgsCardBrand.detect(text, supportedCardBrands)
         return VgsCardNumberTextFieldState(
             text = normalizeText(text, cardBrand),
             fieldName = fieldName,
             validators = validators,
             isCardBrandValidationEnabled = isCardBrandValidationEnabled,
+            supportedCardBrands = supportedCardBrands,
             cardBrand = cardBrand,
         )
     }
@@ -74,6 +83,14 @@ class VgsCardNumberTextFieldState internal constructor(
         val digits = text.filter(Char::isDigit)
         val length = digits.length
         return digits.substring(0, min(length, cardBrand.length.maxOrNull() ?: length))
+    }
+
+    private fun generateBin(): String? {
+        return if (isValid) text.substring(0, min(text.length, 6)) else null
+    }
+
+    private fun generateLast4(): String? {
+        return if (isValid) text.substring(max(0, text.length - 4), text.length) else null
     }
 }
 
