3118930a1b397271ae45441edd98b2cfa1a11f7a
Vadym Bobryshev <vadym.bobryshev@vgs.io>
2025-12-02 01:46:11 +0200

    add ABA routing

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/ABARoutingNumberValidator.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/ABARoutingNumberValidator.kt
@@ -1,2 +1,26 @@
-package com.verygoodsecurity.vgscollect.view.card.validation 
+package com.verygoodsecurity.vgscollect.view.card.validation
 
+/**
+ * ABA Routing Number checksum validation.
+ */
+class ABARoutingNumberValidator(
+    override val errorMsg: String = DEFAULT_ERROR_MSG
+): VGSValidator {
+
+    override fun isValid(content: String): Boolean {
+        if (content.isEmpty() || !content.matches(Regex("^\\d{9}$"))) {
+            return false
+        }
+        val digits = content.map { it.digitToInt() }
+        val sum = 3 * (digits[0] + digits[3] + digits[6]) +
+                7 * (digits[1] + digits[4] + digits[7]) +
+                1 * (digits[2] + digits[5] + digits[8])
+
+        return sum != 0 && sum % 10 == 0
+    }
+
+    internal companion object Companion {
+
+        internal const val DEFAULT_ERROR_MSG = "is not a valid ABA routing number"
+    }
+}
--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/rules/ABARoutingNumberRule.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/view/card/validation/rules/ABARoutingNumberRule.kt
@@ -1,4 +1,39 @@
 package com.verygoodsecurity.vgscollect.view.card.validation.rules
 
-class ABARoutingNumberRule {
+import com.verygoodsecurity.vgscollect.view.card.validation.ABARoutingNumberValidator
+import com.verygoodsecurity.vgscollect.view.card.validation.LengthValidator
+
+class ABARoutingNumberRule private constructor(
+    private val lengthValidator: LengthValidator,
+    private val checksumValidator: ABARoutingNumberValidator
+) : ValidationRule(null, null, null, null) {
+
+    class ValidationBuilder {
+
+        private var errorMsg: String? = null
+
+        /**
+         * Sets the error message to be displayed if the validation fails.
+         *
+         * @param errorMsg The error message.
+         */
+        fun setErrorMsg(errorMsg: String) = this.apply {
+            this.errorMsg = errorMsg
+        }
+
+        /**
+         * Builds the validation rule.
+         */
+        fun build(): ABARoutingNumberRule {
+            val errorMessage = this.errorMsg ?: "ABA routing number is not valid"
+            return ABARoutingNumberRule(
+                LengthValidator(ABA_ROUTING_NUMBER_LENGTH, ABA_ROUTING_NUMBER_LENGTH, errorMessage),
+                ABARoutingNumberValidator(errorMessage)
+            )
+        }
+    }
+
+    companion object {
+        private const val ABA_ROUTING_NUMBER_LENGTH = 9
+    }
 }
