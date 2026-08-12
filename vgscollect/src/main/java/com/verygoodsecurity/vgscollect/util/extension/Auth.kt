75484229c708f0085d5475612e088f536c4d5656
dmytro.dmytryshyn <dmytro.dmytryshyn@vgs.io>
2026-08-12 17:03:55 +0300

    Fix bearer

--- vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/Auth.kt
+++ vgscollect/src/main/java/com/verygoodsecurity/vgscollect/util/extension/Auth.kt
@@ -4,5 +4,6 @@ private const val AUTH_HEADER_KEY = "Authorization"
 private const val AUTH_HEADER_VALUE = "Bearer"
 
 internal fun String.toAuthHeader(): Pair<String, String> {
-    return AUTH_HEADER_KEY to "$AUTH_HEADER_VALUE $this"
+    val token = if (startsWith("$AUTH_HEADER_VALUE ")) this else "$AUTH_HEADER_VALUE $this"
+    return AUTH_HEADER_KEY to token
 }
